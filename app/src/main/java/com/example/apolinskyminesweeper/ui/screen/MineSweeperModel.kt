360ba681d0a00cd557b20465a744046ae17c0f8d
Charlie Apolinsky <charlie.apolinsky@gmail.com>
2024-09-29 21:18:46 +0200

    [Implementation] Implemented logic for auto clearing 0's

--- app/src/main/java/com/example/apolinskyminesweeper/ui/screen/MineSweeperModel.kt
+++ app/src/main/java/com/example/apolinskyminesweeper/ui/screen/MineSweeperModel.kt
@@ -16,15 +16,16 @@ class MineSweeperModel : ViewModel() {
     var flagMode by mutableStateOf(false)
     var isGameOver by mutableStateOf(false)
     var isGameWon by mutableStateOf(false)
+    final val BOARD_SIZE = 5
 
     fun startGame() {
         // Initialize the board and solution board
-        board = Array(5) { Array(5) { "" } }
-        solutionBoard = Array(5) { Array(5) { "" } }
+        board = Array(BOARD_SIZE) { Array(5) { "" } }
+        solutionBoard = Array(BOARD_SIZE) { Array(BOARD_SIZE) { "" } }
         var mineCount = 3
         while (mineCount > 0) {
-            val x = Random.nextInt(5)
-            val y = Random.nextInt(5)
+            val x = Random.nextInt(BOARD_SIZE)
+            val y = Random.nextInt(BOARD_SIZE)
             if (solutionBoard[x][y] == "") {
                 solutionBoard[x][y] = "mine"
                 mineCount--
@@ -39,7 +40,7 @@ class MineSweeperModel : ViewModel() {
     fun onCellClick(row: Int, col: Int) {
         if (!flagMode) {
             if (solutionBoard[row][col] == "") {
-                getAdjacentMines(row, col)
+                board[row][col] = getAdjacentMines(row, col).toString()
             } else if (solutionBoard[row][col] == "mine") {
                 endGame()
             }
@@ -73,17 +74,53 @@ class MineSweeperModel : ViewModel() {
         flagMode = !flagMode
     }
 
-    fun getAdjacentMines(row: Int, col: Int) {
+    fun getAdjacentMines(row: Int, col: Int): Int{
         var mineCount = 0
         for (i in (row - 1)..(row + 1)) {
             for (j in (col - 1)..(col + 1)) {
-                if ((i in 0..4 && j in 0..4) && (solutionBoard[i][j] == "mine")) {
+                if ((i in 0..(BOARD_SIZE - 1) && j in 0..(BOARD_SIZE - 1)) && (solutionBoard[i][j] == "mine")) {
                     mineCount += 1
                 }
             }
         }
-        board[row][col] = mineCount.toString()
+        if (mineCount == 0) {
+            val isRevealed = Array(BOARD_SIZE) { Array(BOARD_SIZE) { false } }
+            revealEmptyArea(row, col, isRevealed)
+        }
         Log.d("Board", board.contentDeepToString())
+        return mineCount
+    }
+
+    fun revealEmptyArea(row: Int, col: Int, isRevealed: Array<Array<Boolean>> = Array(BOARD_SIZE) { Array(BOARD_SIZE) { false } }) {
+        isRevealed[row][col] = true
+        for (i in (row - 1)..(row + 1)) {
+            for (j in (col - 1)..(col + 1)) {
+                if ((i in 0..(BOARD_SIZE - 1) && j in 0..(BOARD_SIZE - 1)) && (solutionBoard[i][j] == "mine")) {
+                    return
+                }
+            }
+        }
+        board[row][col] = "0"
+        if (row + 1 < BOARD_SIZE) {
+            if (!isRevealed[row + 1][col]) {
+                revealEmptyArea(row + 1, col, isRevealed)
+            }
+        }
+        if (row - 1 >= 0) {
+            if (!isRevealed[row - 1][col]) {
+                revealEmptyArea(row - 1, col, isRevealed)
+            }
+        }
+        if (col + 1 < BOARD_SIZE) {
+            if (!isRevealed[row][col + 1]) {
+                revealEmptyArea(row, col + 1, isRevealed)
+            }
+        }
+        if (col - 1 >= 0) {
+            if (!isRevealed[row][col - 1]) {
+                revealEmptyArea(row, col - 1, isRevealed)
+            }
+        }
     }
 
     fun endGame() {
